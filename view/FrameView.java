85f860e5f7c411e46e7f1f8735de7eacf5108abb
valeriobattipaglia <v.battipaglia12@studenti.unisa.it>
2022-11-22 21:56:35 +0100

    add app icon

--- view/FrameView.java
+++ view/FrameView.java
@@ -12,10 +12,10 @@ public class FrameView {
     public static void createView(){
         JFrame frame = new JFrame("Drawing software");
         frame.setSize(600,600);
-        CanvasView canvas = new CanvasView(new Drawing());
+        CanvasView canvas = new CanvasView(new Drawing());                  //create canvas for draw
         frame.add(canvas);
         JPanel vPanel = new JPanel();
-        BoxLayout boxButton = new BoxLayout(vPanel, BoxLayout.Y_AXIS);
+        BoxLayout boxButton = new BoxLayout(vPanel, BoxLayout.Y_AXIS);      //create a "vbox " for  tools
         vPanel.setLayout(boxButton);
         vPanel.add(new JButton(new ImageIcon("assets/icons/cursor.png")));
         vPanel.add(Box.createRigidArea(new Dimension(0, 5)));
@@ -26,6 +26,8 @@ public class FrameView {
         vPanel.add(new JButton(new ImageIcon("assets/icons/ellipse.png") ));
         vPanel.add(Box.createRigidArea(new Dimension(0, 5)));
         frame.add(vPanel, BorderLayout.WEST);
+        ImageIcon icon = new ImageIcon("assets/icons/palette.png");          //app icon
+        frame.setIconImage(icon.getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);//making the frame visible
 
