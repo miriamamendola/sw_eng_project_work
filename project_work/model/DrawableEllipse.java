3b9aefd8d7e88a6256c9d5c288858179bb6afda6
Antonio <a.caso35@studenti.unisa.it>
2022-11-23 16:40:57 +0100

    Modified classes by working both on a second constructor (recalling the other one) and added methods "equals" and "setSize".

--- project_work/model/DrawableEllipse.java
+++ project_work/model/DrawableEllipse.java
@@ -2,19 +2,21 @@ package project_work.model;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.beans.PropertyChangeSupport;
+import java.util.Objects;
 
 public class DrawableEllipse extends Ellipse2D.Double implements Drawable{
     private Paint fillColor;
     private Paint strokeColor;
 
-    public DrawableEllipse(Paint fillColor, Paint strokeColor) {
+    public DrawableEllipse(Paint fillColor, Paint strokeColor, double x, double y) {
+        super(x,y,0,0);
         this.fillColor = fillColor;
         this.strokeColor = strokeColor;
     }
 
-    public DrawableEllipse() {
-        this.fillColor = Color.white;
-        this.strokeColor = Color.black;
+    public DrawableEllipse(double x, double y) {
+        this(Color.white, Color.black, x, y);
     }
 
     @Override
@@ -23,5 +25,13 @@ public class DrawableEllipse extends Ellipse2D.Double implements Drawable{
         g2d.fill(this);
         g2d.setPaint(strokeColor);
         g2d.draw(this);
+    }
+    @Override
+    public boolean equals(Object o) {
+        return this == o;
+    }
+
+    public void setSize(double w, double h){
+        this.setFrame(this.getX(), this.getY(), w, h);
     }
 }
--- project_work/model/DrawableLine.java
+++ project_work/model/DrawableLine.java
@@ -2,22 +2,28 @@ package project_work.model;
 
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 
 public class DrawableLine extends Line2D.Double implements Drawable  {
     private Paint strokeColor;
 
-    public DrawableLine(Paint strokeColor) {
+    public DrawableLine(Paint strokeColor, Point2D p1, Point2D p2) {
+        super(p1, p2);
         this.strokeColor = strokeColor;
     }
 
-    public DrawableLine() {
-        this.strokeColor = Color.black;
+    public DrawableLine(Point2D p1, Point2D p2) {
+        this(Color.black, p1, p2);
     }
-
 
     @Override
     public void draw(Graphics2D g2d) {
         g2d.setPaint(strokeColor);
         g2d.draw(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return this == o;
     }
 }
