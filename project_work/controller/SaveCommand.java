9d2fb3d2e9ecf0f3dbeffe51e8ae399f735961ee
gdellorto1 <g.dellorto1@studenti.unisa.it>
2022-11-25 11:25:42 +0100

    class SaveCommand

---
+++ project_work/controller/SaveCommand.java
@@ -0,0 +1,28 @@
+package project_work.controller;
+
+import project_work.view.CanvasView;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SaveCommand implements Command{
+
+    private final CanvasView canvas;
+    private File file;
+    public SaveCommand(CanvasView canvas, File file){
+        this.canvas = canvas;
+        this.file = file;
+    }
+
+    @Override
+    public void execute() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))){
+            oos.writeObject(canvas.getDrawing());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
