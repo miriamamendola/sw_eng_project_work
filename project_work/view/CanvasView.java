06bda732d11bafcd55ef8517535497d52d84e3d1
gdellorto1 <g.dellorto1@studenti.unisa.it>
2022-11-25 10:48:08 +0100

    Drawing using antialiasing feature

--- project_work/view/CanvasView.java
+++ project_work/view/CanvasView.java
@@ -67,15 +67,21 @@ public class CanvasView extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
 
-        g.clearRect(0,0, this.getWidth(), this.getHeight());
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHints(rh);
+
+        g2d.clearRect(0,0, this.getWidth(), this.getHeight());
 
         for (Drawable d : this.drawing) {
-            d.draw((Graphics2D) g);
+            d.draw(g2d);
         }
 
         if(dummyDrawable != null){
-            dummyDrawable.draw((Graphics2D) g);
+            dummyDrawable.draw(g2d);
         }
     }
 
