59d2bd5c5974f764fe98bd9561d0e04d9a89b3b9
miriamamendola <m.amendola21@studenti.unisa.it>
2022-11-25 23:14:55 +0100

    Fixed bug on Rectangle and Ellipse creation.

--- project_work/controller/EllipseTool.java
+++ project_work/controller/EllipseTool.java
@@ -4,6 +4,10 @@ import project_work.model.DrawableEllipse;
 import project_work.view.CanvasView;
 
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
 
 public class EllipseTool implements Tool {
 
@@ -13,6 +17,8 @@ public class EllipseTool implements Tool {
 
     private final Invoker invoker;
 
+    private Point2D startingPoint;
+
     public EllipseTool(CanvasView canvas, Invoker invoker){
         this.canvas = canvas;
         this.invoker = invoker;
@@ -20,16 +26,20 @@ public class EllipseTool implements Tool {
 
     @Override
     public void mouseDragged(MouseEvent mouseEvent) {
-        double width = mouseEvent.getX() - ellipse.getX();
-        double height = mouseEvent.getY() - ellipse.getY();
-        ellipse.setSize(width, height);
+        double x = min(startingPoint.getX(), mouseEvent.getX());
+        double y = min(startingPoint.getY(), mouseEvent.getY());
+        double width = abs(startingPoint.getX() - mouseEvent.getX());
+        double height = abs(startingPoint.getY() - mouseEvent.getY());
+
+        ellipse.setFrame(x, y, width, height);
+        canvas.setDummyDrawable(ellipse);
         canvas.repaint();
     }
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
+        this.startingPoint = mouseEvent.getPoint();
         ellipse = new DrawableEllipse(mouseEvent.getX(), mouseEvent.getY());
-        canvas.setDummyDrawable(ellipse);
     }
 
     @Override
--- project_work/controller/RectangleTool.java
+++ project_work/controller/RectangleTool.java
@@ -4,6 +4,10 @@ import project_work.model.DrawableRectangle;
 import project_work.view.CanvasView;
 
 import java.awt.event.MouseEvent;
+import java.awt.geom.Point2D;
+
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
 
 public class RectangleTool implements Tool {
 
@@ -13,29 +17,35 @@ public class RectangleTool implements Tool {
 
     private final Invoker invoker;
 
-    public RectangleTool(CanvasView canvas, Invoker invoker){
+    private Point2D startingPoint;
+
+    public RectangleTool(CanvasView canvas, Invoker invoker) {
         this.canvas = canvas;
         this.invoker = invoker;
     }
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
-        rect = new DrawableRectangle(mouseEvent.getX(), mouseEvent.getY());
-        canvas.setDummyDrawable(rect);
+        this.rect = new DrawableRectangle(mouseEvent.getX(), mouseEvent.getY());
+        this.startingPoint = mouseEvent.getPoint();
     }
 
     @Override
     public void mouseDragged(MouseEvent mouseEvent) {
-        double width = mouseEvent.getX() - rect.getX();
-        double height = mouseEvent.getY() - rect.getY();
-        rect.setSize(width, height);
+        double x = min(startingPoint.getX(), mouseEvent.getX());
+        double y = min(startingPoint.getY(), mouseEvent.getY());
+        double width = abs(startingPoint.getX() - mouseEvent.getX());
+        double height = abs(startingPoint.getY() - mouseEvent.getY());
+
+        rect.setRect(x, y, width, height);
+        canvas.setDummyDrawable(rect);
         canvas.repaint();
     }
 
 
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
-        if (rect.getWidth() > 0 && rect.getHeight() > 0){
+        if (rect.getWidth() > 0 && rect.getHeight() > 0) {
             invoker.executeCommand(new ShapeCommand(canvas, rect));
         }
         canvas.clearDummyDrawable();
