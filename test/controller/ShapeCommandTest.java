666413c4fc362b687df84ace4887d1e511b38294
gdellorto1 <g.dellorto1@studenti.unisa.it>
2022-11-26 17:09:23 +0100

    Added ShapeCommandTest for testing ShapeCommand class.

---
+++ test/controller/ShapeCommandTest.java
@@ -0,0 +1,49 @@
+package test.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import project_work.controller.ShapeCommand;
+import project_work.model.Drawable;
+import project_work.model.DrawableEllipse;
+import project_work.model.DrawableLine;
+import project_work.model.DrawableRectangle;
+import project_work.view.CanvasView;
+
+import java.awt.geom.Point2D;
+
+import static org.junit.Assert.*;
+
+public class ShapeCommandTest {
+
+    private Drawable shape;
+    private CanvasView canvas;
+
+    @Before
+    public void setUp() {
+        canvas = new CanvasView();
+    }
+
+    @Test
+    public void testExecuteWithLine() {
+        Point2D p1 = new Point2D.Double(10, 10);
+        Point2D p2 = new Point2D.Double(20, 20);
+        shape = new DrawableLine(p1, p2);
+        new ShapeCommand(canvas,shape).execute();
+        assertTrue(canvas.getDrawing().containsDrawable(shape));
+    }
+
+    @Test
+    public void testExecuteWithRectangle() {
+        shape = new DrawableRectangle(10, 10);
+        new ShapeCommand(canvas,shape).execute();
+        assertTrue(canvas.getDrawing().containsDrawable(shape));
+    }
+
+    @Test
+    public void testExecuteWithEllipse() {
+        shape = new DrawableEllipse(10, 10);
+        new ShapeCommand(canvas,shape).execute();
+        assertTrue(canvas.getDrawing().containsDrawable(shape));
+    }
+
+}
