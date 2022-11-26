65c247df7c8f1e43d782cce1987c96792da5fd99
Antonio <a.caso35@studenti.unisa.it>
2022-11-26 16:59:16 +0100

    Added selectedShape property, added getter and setter.

--- project_work/model/SelectionGrid.java
+++ project_work/model/SelectionGrid.java
@@ -3,17 +3,26 @@ package project_work.model;
 import java.awt.*;
 
 public class SelectionGrid extends DrawableRectangle {
+    private Shape selectedShape;
     Stroke dashed = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
 
     public SelectionGrid(Shape selectedShape) {
-        super(null, Color.blue, selectedShape.getBounds().getX(), selectedShape.getBounds().getY());
+        super(null, Color.gray, selectedShape.getBounds().getX(), selectedShape.getBounds().getY());
         this.setRect(selectedShape.getBounds().getX() - 1, selectedShape.getBounds().getY() - 1, selectedShape.getBounds().getWidth() + 2, selectedShape.getBounds().getHeight() + 2);
+        this.selectedShape = selectedShape;
     }
 
     @Override
     public void draw(Graphics2D g2d) {
         g2d.setStroke(dashed);
-        g2d.setPaint(Color.gray);
-        g2d.draw(this);
+        super.draw(g2d);
+    }
+
+    public Shape getSelectedShape() {
+        return selectedShape;
+    }
+
+    public void setSelectedShape(Shape selectedShape) {
+        this.selectedShape = selectedShape;
     }
 }
