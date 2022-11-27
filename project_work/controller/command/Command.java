28e5b7980f97645b0ffb07b98a62fbef244151ff
Antonio <a.caso35@studenti.unisa.it>
2022-11-27 19:59:35 +0100

    Added JavaDoc to Drawable interface.

--- project_work/controller/command/Command.java
+++ project_work/controller/command/Command.java
@@ -1,5 +1,17 @@
 package project_work.controller.command;
 
+/**
+ * First step in implementing the Command design pattern; this interface is to
+ * be then implemented by all the concrete commands that will then overwrite
+ * the method (which is usually devoid of input parameters) execute().
+ */
 public interface Command {
+    /**
+     * When implemented by the ConcreteCommands, will allow the Invoker to
+     * be independent of how the client's request is performed; that is, the Invoker
+     * will not need to have beforehand knowledge of what command is being requested
+     * and the specific parameters involved. This also allows for the Invoker's behaviour
+     * to be changed at runtime.
+     */
     void execute();
 }
