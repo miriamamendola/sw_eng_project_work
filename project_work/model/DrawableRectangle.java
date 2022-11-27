f58f13878bb1c59ebdfe57d3b13defa052d637fe
Antonio <a.caso35@studenti.unisa.it>
2022-11-27 13:20:11 +0100

    Added javadoc to DrawableRectangle class.

--- project_work/model/DrawableRectangle.java
+++ project_work/model/DrawableRectangle.java
@@ -3,10 +3,20 @@ package project_work.model;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
-
+/**
+ * Defines the actual rectangle shape and it's properties.
+ */
 public class DrawableRectangle extends Rectangle2D.Double implements Drawable {
     private Paint fillColor;
     private Paint strokeColor;
+
+    /**
+     * Creates a rectangle whose properties will be set to the input parameters' values.
+     * @param fillColor is the internal color of the shape.
+     * @param strokeColor is the outline color of the rectangle.
+     * @param x is the first coordinate used to determine the position on the canvas.
+     * @param y is the second coordinate used to determine the position on the canvas.
+     */
 
     public DrawableRectangle(Paint fillColor, Paint strokeColor, double x, double y) {
         super(x, y, 0, 0);
@@ -14,10 +24,22 @@ public class DrawableRectangle extends Rectangle2D.Double implements Drawable {
         this.strokeColor = strokeColor;
     }
 
+    /**
+     * Creates a rectangle in which the fillColor and strokeColor properties will be set to default values,
+     * which are TRANSPARENT for the fill and black for the stroke.
+     * @param x is the first coordinate used to determine the position on the canvas.
+     * @param y is the second coordinate used to determine the position on the canvas.
+     */
     public DrawableRectangle(double x, double y) {
         this(null, Color.black, x, y);
     }
 
+    /**
+     * Allows to draw che input parameter object by calling the parent's draw method.
+     * If the default constructor is used, and the fill is to be transparent, then there is no need
+     * to call the fill() method at all.
+     * @param g2d is the Graphics2D object to be drawn.
+     */
     @Override
     public void draw(Graphics2D g2d) {
         if(fillColor != null) {
@@ -27,7 +49,14 @@ public class DrawableRectangle extends Rectangle2D.Double implements Drawable {
         g2d.setPaint(strokeColor);
         g2d.draw(this);
     }
-
+    /**
+     * Allows to verify if an input object and a DrawableRectangle one are actually
+     * equivalent to each other. Used in JUnit testing.
+     *
+     * @param o an {@code Object} to be compared with this
+     *          {@code Rectangle2D}.
+     * @return whether the input object is actually equal to the DrawableRectangle object.
+     */
     @Override
     public boolean equals(Object o) {
         return this == o;
