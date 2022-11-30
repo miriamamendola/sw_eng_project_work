451f55e334a2f3b535db6a3951025c3b997e01c9
Antonio <a.caso35@studenti.unisa.it>
2022-11-30 19:18:07 +0100

    Modified Drawing so to have a better-functioning remove() method: it now allows to remove based on a Drawable to remove and not on its index in the list.

--- src/main/java/drawing_software/model/Drawing.java
+++ src/main/java/drawing_software/model/Drawing.java
@@ -39,10 +39,11 @@ public class Drawing implements Serializable, Iterable<Drawable> {
     /**
      * Allows to retrieve (and the remove) a Drawable object previously added to the data structure.
      * This DOES remove the Drawable shape from the structure.
-     * @param index is the index at which the Drawable shape is located.
+     *
+     * @param d is the shape to be deleted from the drawing.
      */
-    public Drawable removeDrawable(int index) {
-        return this.listDrawables.remove(index);
+    public boolean removeDrawable(Drawable d) {
+        return this.listDrawables.remove(d);
     }
 
     /**
