ddf703dc182b1698bf7e738bb8a431d6146480c4
Antonio <a.caso35@studenti.unisa.it>
2022-12-01 17:24:21 +0100

    Added Paste-while-moving functionality.

--- src/main/java/drawing_software/controller/tool/PasteTool.java
+++ src/main/java/drawing_software/controller/tool/PasteTool.java
@@ -1,17 +1,23 @@
 package drawing_software.controller.tool;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import drawing_software.controller.command.Invoker;
 import drawing_software.controller.command.PasteCommand;
 import drawing_software.view.CanvasView;
 
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
 
+import static java.lang.Thread.sleep;
 
-public class PasteTool implements Tool {
+
+public class PasteTool extends JPanel implements Tool {
     private final CanvasView canvas;
     private final Invoker invoker;
+    private boolean c = false;
 
 
     public PasteTool(CanvasView canvas, Invoker invoker) {
@@ -21,8 +27,20 @@ public class PasteTool implements Tool {
 
     @Override
     public void mouseLeftClicked(MouseEvent mouseEvent) {
+        c = true;
         Point2D p = mouseEvent.getPoint();
         invoker.executeCommand(new PasteCommand(canvas, p));
+        c = false;
+    }
+
+    public void mouseDragged(MouseEvent mouseEvent){
+        Point2D p = mouseEvent.getPoint();
+        invoker.executeCommand(new PasteCommand(canvas, p));
+        try {
+            sleep(57);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
