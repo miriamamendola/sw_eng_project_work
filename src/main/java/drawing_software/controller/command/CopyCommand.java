76fb188bea91044f8d4d7e2ef217c4dcac18bdc9
valeriobattipaglia <v.battipaglia12@studenti.unisa.it>
2022-12-03 11:35:09 +0100

    add copy test for figure and insert comments

--- src/main/java/drawing_software/controller/command/CopyCommand.java
+++ src/main/java/drawing_software/controller/command/CopyCommand.java
@@ -8,15 +8,26 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.Transferable;
 
+/**
+ * Implements a ConcreteCommand class; in this case, the Command is the Copy command.
+ */
 public class CopyCommand implements Command ,ClipboardOwner{
 
     private final CanvasView canvas;
 
+    /**
+     * @param canvas is the reference to the receiver which will perform the required action.
+     */
 
     public CopyCommand(CanvasView canvas) {
         this.canvas = canvas;
     }
 
+    /**
+     * First,it takes the selected figure to copy, and then it creates a clipboard where save the figure.
+     * The operation is completed by adding the shapes in the canvas and repaint the canvas in order to can paste easily .
+     *
+     */
     @Override
     public void execute() {
         Drawable selectedShape = canvas.getSelectionGrid().getSelectedShape();
@@ -28,6 +39,9 @@ public class CopyCommand implements Command ,ClipboardOwner{
 
     }
 
+    /**
+     * This method is necessary for use a clipboard
+     */
     @Override
     public void lostOwnership(Clipboard clipboard, Transferable contents) {
         System.out.println("ClipboardTest: Lost ownership");
